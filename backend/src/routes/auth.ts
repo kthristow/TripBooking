fc3c33a3d25f57f3b1963f2452b559a3b3777210
Kaloyan <79564253+kthristow@users.noreply.github.com>
2024-04-21 07:57:27 +0300

    Create App context
    Create app context.
    Create Toast message component.
    Create the registration logic and page.

---
+++ backend/src/routes/auth.ts
@@ -0,0 +1,58 @@
+import express, { Request, Response } from "express";
+import { check, validationResult } from "express-validator";
+import User from "../models/user";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const router = express.Router();
+
+router.post(
+  "/login",
+  [
+    check("email", "Email is required").isEmail(),
+    check("password", "Password with 6 or more characters required").isLength({
+      min: 6,
+    }),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ message: errors.array() });
+    }
+
+    const { email, passsword } = req.body;
+
+    try {
+      const user = await User.findOne({ email });
+      if (!user) {
+        //the message is equal because we give "the hacker" a clue which thing is wrong
+        return res.status(400).json({ message: "Invalid credentials" });
+      }
+
+      const isMatch = await bcrypt.compare(passsword, user.passsword);
+      if (!isMatch) {
+        //the message is equal because we give "the hacker" a clue which thing is wrong
+        return res.status(400).json({ message: "Invalid credentials" });
+      }
+
+      const token = jwt.sign(
+        { userId: user.id },
+        process.env.JWT_SECRET_KEY as string,
+        { expiresIn: "1d" }
+      );
+
+      res.cookie("auth_token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 86400000,
+      });
+
+      res.status(200).json({ userId: user._id });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Something went wrong" });
+    }
+  }
+);
+
+export default router;
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,17 +5,36 @@ import {
   Navigate,
 } from "react-router-dom";
 import Layout from "./layouts/Layout";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Layout>
-          <span>Home Page</span>
-        </Layout>} />
-        <Route path="/search" element={<Layout>
-          <span>Search Page</span>
-        </Layout>} />
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <span>Home Page</span>
+            </Layout>
+          }
+        />
+        <Route
+          path="/search"
+          element={
+            <Layout>
+              <span>Search Page</span>
+            </Layout>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <Layout>
+              <Register />
+            </Layout>
+          }
+        />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
