2f8ce5052d199c065f105fba2c8c5deff467cf4d
Kaloyan <79564253+kthristow@users.noreply.github.com>
2024-04-22 17:35:37 +0300

    Finish authentication
    Finish the login/register logic and finishing up the pages.

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -3,6 +3,7 @@ import { check, validationResult } from "express-validator";
 import User from "../models/user";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import verifyToken from "../middleware/auth";
 
 const router = express.Router();
 
@@ -20,25 +21,25 @@ router.post(
       return res.status(400).json({ message: errors.array() });
     }
 
-    const { email, passsword } = req.body;
+    const { email, password } = req.body;
 
     try {
       const user = await User.findOne({ email });
       if (!user) {
-        //the message is equal because we give "the hacker" a clue which thing is wrong
-        return res.status(400).json({ message: "Invalid credentials" });
+        return res.status(400).json({ message: "Invalid Credentials" });
       }
 
-      const isMatch = await bcrypt.compare(passsword, user.passsword);
+      const isMatch = await bcrypt.compare(password, user.password);
       if (!isMatch) {
-        //the message is equal because we give "the hacker" a clue which thing is wrong
-        return res.status(400).json({ message: "Invalid credentials" });
+        return res.status(400).json({ message: "Invalid Credentials" });
       }
 
       const token = jwt.sign(
         { userId: user.id },
         process.env.JWT_SECRET_KEY as string,
-        { expiresIn: "1d" }
+        {
+          expiresIn: "1d",
+        }
       );
 
       res.cookie("auth_token", token, {
@@ -46,7 +47,6 @@ router.post(
         secure: process.env.NODE_ENV === "production",
         maxAge: 86400000,
       });
-
       res.status(200).json({ userId: user._id });
     } catch (error) {
       console.log(error);
@@ -54,5 +54,17 @@ router.post(
     }
   }
 );
+
+router.get("/validate-token", verifyToken, (req: Request, res: Response) => {
+  res.status(200).send({ userId: req.userId });
+});
+
+
+router.post("/logout", (req: Request, res: Response) => {
+  res.cookie("auth_token", "", {
+    expires: new Date(0),
+  });
+  res.send();
+});
 
 export default router;
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom";
+import { useAppContext } from "../contexts/AppContext";
+import SignOutButton from "./SignOutButton";
 
 const Header = () => {
+  const { isLoggedIn } = useAppContext();
+
   return (
     <div className="bg-blue-800 py-6">
       <div className="container mx-auto flex justify-between">
@@ -8,10 +12,20 @@ const Header = () => {
           <Link to="/">MernHolidays.com</Link>
         </span>
         <span className="flex space-x-2">
-          <Link
-            to="/sign-in"
-            className="flex items-center bg-white text-blue-600 px-3 font-bold hover:bg-gray-100 hover:text-green-500"
-          >Sign in</Link>
+          {isLoggedIn ? (
+            <>
+              <Link className="flex items-center text-white px-3 font-bold hover:bg-blue-600" to="/my-bookings">My Bookings</Link>
+              <Link className="flex items-center text-white px-3 font-bold hover:bg-blue-600" to="/my-hotels">My Hotels</Link>
+              <SignOutButton />
+            </>
+          ) : (
+            <Link
+              to="/sign-in"
+              className="flex items-center bg-white text-blue-600 px-3 font-bold hover:bg-gray-100 hover:text-green-500"
+            >
+              Sign in
+            </Link>
+          )}
         </span>
       </div>
     </div>
